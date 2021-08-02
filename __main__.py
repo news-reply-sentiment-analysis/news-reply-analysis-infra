507fa210b9986a1be60b2f5155bf3c9b67326389
xoxwgys56 <xoxwgys56@cs.gwnu.ac.kr>
2021-08-02 01:14:51 +0000

    feat: add stack prod

--- __main__.py
+++ __main__.py
@@ -3,8 +3,12 @@
 import pulumi
 from pulumi_aws import s3
 
+
+stack_name = pulumi.get_stack()
+
 # Create an AWS resource (S3 Bucket)
 bucket = s3.Bucket('my-bucket')
 
 # Export the name of the bucket
 pulumi.export('bucket_name', bucket.id)
+
